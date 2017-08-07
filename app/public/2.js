6d9343b2d252842fa5e4a8ac3424956be046ef0d
式燕 <wenjian.ywj@alibaba-inc.com>
2017-08-08 00:14:00 +0800

    add cros

---
+++ app/public/2.js
@@ -0,0 +1,9 @@
+var xhr = new XMLHttpRequest()
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    console.log(JSON.parse(xhr.responseText).msg)
+  }
+}
+// xhr.withCredentials = true
+xhr.open('GET', 'http://x.stuq.com:7001/cros')
+xhr.send(null)
