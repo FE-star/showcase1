56b0f60e4006801e2534cd4848f14aad9493a42d
式燕 <wenjian.ywj@alibaba-inc.com>
2017-08-08 10:29:25 +0800

    update

--- app/router.js
+++ app/router.js
@@ -1,4 +1,4 @@
 module.exports = app => {
-  app.get('/json', app.jsonp({ callback: 'callback' }), app.controller.jsonp.index)
+  app.get('/json', app.jsonp({ callback: 'callback' }), app.controller.json.index)
   app.get('/cros', app.controller.cros.index)
 }
