7d2e6a2504448f82f483b10bfe19c08b74c2516a
loskael <yorts52@qq.com>
2019-08-04 14:55:35 +0800

    fix: 更新 CORS header 设置

--- app/controller/cros.js
+++ app/controller/cros.js
@@ -2,9 +2,13 @@ module.exports = app => {
   class CrosController extends app.Controller {
     * index(req) {
       // https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Access_control_CORS
-      this.ctx.set('Access-Control-Allow-Origin', '*')
-      // this.ctx.set('Access-Control-Allow-Origin', 'http://x.localhost.com')
       // 如果我们要 http://*.localhost.com 都支持跨域怎么办？
+      this.ctx.set('Access-Control-Allow-Origin', '*')
+      // Credentials mode
+      // this.ctx.set('Access-Control-Allow-Origin', 'http://y.localhost.com:7001')
+      // this.ctx.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PUT');
+      // this.ctx.set('Access-Control-Allow-Headers', 'X-Requested-With, User-Agent, Referer, Content-Type, Cache-Control');
+      // this.ctx.set('Access-Control-Allow-Credentials', 'true');
       this.ctx.body = { msg: 'hello world' }
     }
   }
