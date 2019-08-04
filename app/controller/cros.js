15d92dd505e6d961b972dfab53104c7fec2497e4
loskael <yorts52@qq.com>
2019-08-04 14:50:23 +0800

    fix: update hostname

--- app/controller/cros.js
+++ app/controller/cros.js
@@ -4,7 +4,7 @@ module.exports = app => {
       // https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Access_control_CORS
       this.ctx.set('Access-Control-Allow-Origin', '*')
       // this.ctx.set('Access-Control-Allow-Origin', 'http://x.localhost.com')
-      // 如果我们要 http://*.qq.com 都支持跨域怎么办？
+      // 如果我们要 http://*.localhost.com 都支持跨域怎么办？
       this.ctx.body = { msg: 'hello world' }
     }
   }
